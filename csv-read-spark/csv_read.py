745633188e030a350a2ac507c268523cefe4f177
Ajaya Puri Sann <33626260+puriajaya@users.noreply.github.com>
2018-11-21 06:26:49 +0545

    Update csv_read.py

--- csv-read-spark/csv_read.py
+++ csv-read-spark/csv_read.py
@@ -8,7 +8,7 @@ from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
+        print("Usage: file-read", file=sys.stderr)
         sys.exit(-1)
 
     spark = SparkSession\
